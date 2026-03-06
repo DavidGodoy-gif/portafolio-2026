a29167fa70c75896719c73304e64758a8d130d61
David Godoy <modelodga@gmail.com>
2026-03-06 10:36:09 -0300

    formateo texto contenido

--- portfolio/app/casos/[slug]/page.tsx
+++ portfolio/app/casos/[slug]/page.tsx
@@ -1,5 +1,46 @@
 import MetricsChart from '@/components/MetricsChart'
 import { client } from "@/lib/sanity";
+import { PortableText } from "@portabletext/react";
+import { style } from 'framer-motion/client';
+import { h3 } from 'framer-motion/m';
+
+const styles = {
+  h2: {
+    fontSize: '1.2rem',
+    fontWeight: 'bold',
+    marginTop: '1.5rem',
+    marginBottom: '0.5rem',
+  },
+  h3: {
+    fontSize: '1rem',
+    fontWeight: 'bold',
+    marginTop: '1rem',
+    marginBottom: '0.3rem',
+  },
+  ul: {
+    listStyleType: 'disc',
+    marginLeft: '1.5rem',
+    marginBottom: '1rem',
+  },
+  li: {
+    marginBottom: '0.3rem',
+  },
+}
+
+const components = {
+  block: {
+    h2: (props: any) => <h2 style={styles.h2}>{props.children}</h2>,
+    h3: (props: any) => <h3 style={styles.h3}>{props.children}</h3>,
+  },
+  list: {
+    bullet: (props: any) => <ul style={styles.ul}>{props.children}</ul>,
+    number: (props: any) => <ol style={styles.ul}>{props.children}</ol>,
+  },
+  listItem: {
+    bullet: (props: any) => <li style={styles.li}>{props.children}</li>,
+    number: (props: any) => <li style={styles.li}>{props.children}</li>,
+  },
+}
 
 async function getCaso(slug: string) {
   return client.fetch(
@@ -35,11 +76,9 @@ export default async function Caso({
       <section className="mt-16 max-w-3xl">
         <h2 className="text-2xl font-semibold mb-4">Proceso</h2>
 
-        {caso.process.map((block: any, i: number) => (
-          <div key={i} className="mb-4 text-neutral-400">
-            {block.children?.[0]?.text}
-          </div>
-        ))}
+        <div className="text-neutral-400">
+          <PortableText value={caso.process} components={components} />
+        </div>
       </section>
     )}
     {caso?.metrics && (
